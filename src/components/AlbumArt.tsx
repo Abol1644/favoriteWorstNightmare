d074ae6eb03891ffa3dc5a10cf058a7c47f372f2
Abolfazl Bagheri <82612321+Abol1644@users.noreply.github.com>
2026-02-16 04:00:49 +0330

    fixed cover and audios

--- src/components/AlbumArt.tsx
+++ src/components/AlbumArt.tsx
@@ -10,7 +10,7 @@ interface Props {
 const AlbumArt: React.FC<Props> = ({ imageUrl, title, artist = 'Arctic Monkeys' }) => {
   return (
     <div className="album-art">
-      <img src={imageUrl} alt={title} className="album-art__image" />
+      <img src={'https://amg.storage.iran.liara.space/album-cover.jpg'} alt={title} className="album-art__image" />
       <div className="album-art__overlay">
         <h2 className="album-art__title">{title}</h2>
         <p className="album-art__artist">{artist}</p>
